f34e0be7ee5e355a3f196edff1328b39af2d196d
attkauppi <ari.kauppi@helsinki.fi>
2020-03-24 23:56:29 +0200

    v2t2 saldoAlussaOikein ja lataaminenKasvattaaSaldoaOikein

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -12,9 +12,27 @@ public class MaksukorttiTest {
     public void setUp() {
         kortti = new Maksukortti(10);
     }
+    
+    @Test
+    public void saldoAlussaOikein() {
+        assertEquals("Kortilla on rahaa 10.0 euroa", kortti.toString());
+    }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void lataaminenKasvattaaSaldoaOikein() {
+        
+        Maksukortti kortti1 = new Maksukortti(10);
+        Maksukortti kortti2 = new Maksukortti(10);
+        
+        kortti1.lataaRahaa(160);
+        assertEquals("Kortilla on rahaa 150.0 euroa", kortti1.toString());
+        
+        kortti2.lataaRahaa(10);
+        assertEquals("Kortilla on rahaa 20.0 euroa", kortti2.toString());
     }
 }
